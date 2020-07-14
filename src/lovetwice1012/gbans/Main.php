327d6374eda0a23b1fc8743bebcc1db7aafd25e7
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2020-07-15 08:35:30 +0900

    Update Main.php

--- src/lovetwice1012/gbans/Main.php
+++ src/lovetwice1012/gbans/Main.php
@@ -17,7 +17,7 @@ class Main extends PluginBase implements Listener
 {
     public $data;
     public $plugin;
-    public $cver = "1.4.0";
+    public $cver = "1.5.0";
     public $alert = false;
     public $message;
     public function onEnable()
@@ -54,8 +54,11 @@ class Main extends PluginBase implements Listener
 			 $this->alert=true;   
 			    $this->getLogger()->info(Color::RED . "[GBan]とても重要なアップデートがあります。アップデートしないと、GBanの動作に致命的な影響を及ぼす可能性があります。すぐにアップデートをしてください。");
 		    }else{
+			    $this->alert=false;  
 			    $this->getLogger()->info(Color::RED . "[GBan]新しいバージョンがあります。アップデートしてください。");
 		    }
+	    }else{
+		$this->alert=false;      
 	    }
 }
     public function onPreLogin(PlayerPreLoginEvent $event){
@@ -93,8 +96,11 @@ class Main extends PluginBase implements Listener
 			 $this->alert=true;   
 			    $this->getLogger()->info(Color::RED . "[GBan]とても重要なアップデートがあります。アップデートしないと、GBanの動作に致命的な影響を及ぼす可能性があります。すぐにアップデートをしてください。");
 		    }else{
+			    $this->alert=false;  
 			    $this->getLogger()->info(Color::RED . "[GBan]新しいバージョンがあります。アップデートしてください。");
 		    }
+	    }else{
+		$this->alert=false;      
 	    }
     
 
@@ -104,7 +110,7 @@ class Main extends PluginBase implements Listener
 	}
     public function isbanned($name){
 
-        $url = 'http://passionalldb.s1008.xrea.com/gban/check.php';
+        $url = 'http://passionalldb.s1008.xrea.com/gban/check3.php';
 
         $data = array(
             'check' => 'check',
@@ -145,10 +151,12 @@ class Main extends PluginBase implements Listener
             if ($player instanceof Player){
                 $player->kick("§4あなたはbanされました。 \n§6理由 §f: §6$args[1] ", false);
             }
-                $sender->sendMessage("グローバルbanしました。");
+		  $sender->sendMessage($this->message);  
+                //$sender->sendMessage("グローバルbanしました。");
                 return true;
             }else{
-		    $sender->sendMessage("グローバルbanできませんでした。このサーバーからやあなたからのBAN申請がブロックされているか、サーバーがサービスの提供を一時停止している、もしくはサーバー側でエラーが発生した可能性があります。Gbanプラグインを最新版にアップデートすると解決する場合があります。それでも解決しない場合はしばらく時間をおくか、公式discord-bot「GBans-official」を使用してBanを試みてください。");
+		    $sender->sendMessage($this->message);
+		    //$sender->sendMessage("グローバルbanできませんでした。このサーバーからやあなたからのBAN申請がブロックされているか、サーバーがサービスの提供を一時停止している、もしくはサーバー側でエラーが発生した可能性があります。Gbanプラグインを最新版にアップデートすると解決する場合があります。それでも解決しない場合はしばらく時間をおくか、公式discord-bot「GBans-official」を使用してBanを試みてください。");
                     return true;
             }
 	    }
@@ -159,11 +167,12 @@ class Main extends PluginBase implements Listener
                 return true;
             }
             if($this->unban($args[0],$sender->getName())){
-           
-                $sender->sendMessage("グローバルunbanしました。");
+           $sender->sendMessage($this->message);
+                //$sender->sendMessage("グローバルunbanしました。");
                 return true;
             }else{
-		    $sender->sendMessage("グローバルunbanできませんでした。このサーバーからやあなたからのUNBAN申請がブロックされているか、サーバーがサービスの提供を一時停止している、もしくはサーバー側でエラーが発生した可能性があります。Gbanプラグインを最新版にアップデートすると解決する場合があります。それでも解決しない場合はしばらく時間をおいて再度試してみてください。");
+		     $sender->sendMessage($this->message);
+		    //$sender->sendMessage("グローバルunbanできませんでした。このサーバーからやあなたからのUNBAN申請がブロックされているか、サーバーがサービスの提供を一時停止している、もしくはサーバー側でエラーが発生した可能性があります。Gbanプラグインを最新版にアップデートすると解決する場合があります。それでも解決しない場合はしばらく時間をおいて再度試してみてください。");
                     $sender->sendMessage("§4[注意]UNBANはBANした人本人がUNBANしていて、BANした時にいたサーバーで行わないと拒否されます。");
 		    return true;
             }
@@ -190,6 +199,7 @@ class Main extends PluginBase implements Listener
         );
 
         $result = @file_get_contents($url, false, stream_context_create($context));
+	$this->message = $result;
         if($result=="success"){
             return true;
         }else{
@@ -217,6 +227,7 @@ class Main extends PluginBase implements Listener
         );
 
         $result = @file_get_contents($url, false, stream_context_create($context));
+	    $this->message = $result;
         if($result=="success"){
             return true;
         }else{
